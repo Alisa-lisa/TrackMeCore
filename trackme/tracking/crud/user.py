f59a3b044f93e0f4ed03c507cb45de84d5f956e7
alisa <alisa.dammer@gmail.com>
2021-04-06 21:13:30 +0200

    fix token creation

--- trackme/tracking/crud/user.py
+++ trackme/tracking/crud/user.py
@@ -1,6 +1,7 @@
 from typing import Optional, Tuple
 import uuid
 from sqlalchemy.ext.asyncio.session import AsyncSession
+from sqlalchemy.sql import select 
 from trackme.tracking.types import (
         UserInput,
         UserOutput, 
@@ -37,10 +38,11 @@ async def auth_user(user: UserInput,
     """ create new access token or return existing one for existing user """
     async with async_session() as db:
         verify_user = await _get_user(db, user)
+
         if verify_user is not None:
-            existing_token = await db.query(UserActivityModel)\
+            existing_token = (await db.execute(select(UserActivityModel)\
                     .filter(UserActivityModel.user_id == verify_user.user_id)\
-                    .filter(UserActivityModel.client == client).first()
+                    .filter(UserActivityModel.client == client))).first()
             if existing_token is not None:
                 return str(existing_token.token), "success"
             else:
@@ -61,7 +63,7 @@ async def auth_user(user: UserInput,
 # read
 async def _get_user(db: AsyncSession, user: UserInput) -> Optional[UserOutput]:
     try:
-        existing_user = await db.query(UserModel).filter(UserModel.name == user.name).first()
+        existing_user = (await db.execute(select(UserModel).filter(UserModel.name == user.name))).first()[0]
         if existing_user is not None and verify(existing_user, user):
             return UserOutput(name=existing_user.name, 
                     user_id=existing_user.id)
@@ -73,9 +75,8 @@ async def _get_user(db: AsyncSession, user: UserInput) -> Optional[UserOutput]:
 
 
 async def get_user_by_token(db: AsyncSession, token: str) -> Optional[int]:
-    user_id = await db.query(UserActivityModel.user_id)\
-    .filter(UserActivityModel.token == token)\
-    .first()
+    user_id = (await db.execute(select(UserActivityModel.user_id)\
+    .filter(UserActivityModel.token == token))).first()[0]
     return user_id[0]
 
 
