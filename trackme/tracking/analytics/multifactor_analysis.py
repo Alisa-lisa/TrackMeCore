362e8b897b355df1e0b89a52dc7bfd32a720023b
alisa <alisa.dammer@gmail.com>
2021-12-26 22:18:13 +0100

    quick fix for same-date-multiple-entries

--- trackme/tracking/analytics/multifactor_analysis.py
+++ trackme/tracking/analytics/multifactor_analysis.py
@@ -1,8 +1,8 @@
 """ analysis of interaction of multiple factors """
 from typing import List, Optional
-from trackme.tracking.models.tracking import TrackingActivity as TA
+from trackme.tracking.types.tracking import TrackingActivity as TA
 import scipy.stats as stats
-
+import numpy as np
 
 def pearson_correlation(first_factor: List[TA], second_factor: List[TA]) -> Optional[float]:
     """Pearson coefficient can be seen as global syncrony
@@ -20,8 +20,17 @@ def pearson_correlation(first_factor: List[TA], second_factor: List[TA]) -> Opti
     first_series_dates = [item.created_at.date() for item in first_factor if item is not None]
     second_series_dates = [item.created_at.date() for item in second_factor if item is not None]
     effective_dates = list(set(first_series_dates).intersection(second_series_dates))
-    factor_one = [item.estimation for item in first_factor if item.created_at.date() in effective_dates]
-    factor_two = [item.estimation for item in second_factor if item.created_at.date() in effective_dates]
+    # take average if there are multiple entries for the same day
+    # should be done via sql ideally
+    def _avg_estimation(date, array: List[TA]) -> int:
+        return int(np.mean([item.estimation for item in array if item.created_at.date() == date]))
+    factor_one = []
+    factor_two = []
+    for date in effective_dates:
+        estimation = _avg_estimation(date, first_factor)
+        factor_one.append(estimation)
+        estimation = _avg_estimation(date, second_factor)
+        factor_two.append(estimation)
 
     assert len(factor_one) == len(factor_two)
     # an arbitrary number for comparison of rows: a pattern can be seen after at least 2 weeks
