43ac9beffee40fc98bf7c2e5cdcf1359a940c86d
alisa <alisa.dammer@gmail.com>
2022-02-08 16:22:02 +0100

    exclude fast track records from analysis

--- trackme/tracking/analytics/simple_analysis.py
+++ trackme/tracking/analytics/simple_analysis.py
@@ -1,6 +1,6 @@
 """ 1st phase of the analysis """
 from trackme.tracking.analytics.multifactor_analysis import pearson_correlation, pointbiserial_correlation
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 from trackme.tracking.crud.tracking import filter_entries, get_time_horizon, collect_attributes_ids
 from trackme.tracking.crud.tracking_validation import is_attribute_binary
 from trackme.tracking.models.tracking import TrackingActivity as TA
@@ -8,18 +8,25 @@ import statsmodels
 import statsmodels.api as sm
 from statsmodels.stats.stattools import durbin_watson
 import numpy as np
+from fastapi.logger import logger
+import math
 
 
-def detect_autocorrelation(input_data: List[int]) -> Tuple[bool, List[float]]:
+def detect_autocorrelation(input_data: List[int]) -> Tuple[bool, Optional[List[float]]]:
     """
     Compute auto correlation for up to max_lag day difference
     Durbin-Watson test is for lag of 1 (0-4 value)
     H0 - errors are serially uncorrelated
     d = 2 indicates no autocorrelation, << 2 substantial positive, >> substantial negative
     """
-    is_autocor = True if durbin_watson(input_data) != 2 else False
-    auocor = statsmodels.tsa.stattools.pacf(input_data, nlags=7, alpha=0.01)[0].tolist()
-    return is_autocor, auocor
+    try:
+        is_autocor = True if durbin_watson(input_data) != 2 else False
+        autocor = statsmodels.tsa.stattools.pacf(input_data, nlags=7, alpha=0.01)[0].tolist()
+    except Exception as ex:
+        logger.error(f"Couldn't compute autocorrelation due to {ex}")
+        autocor = None
+        is_autocor = False
+    return is_autocor, autocor
 
 
 def detect_trend(input_data: List[int]) -> List[float]:
@@ -99,7 +106,6 @@ async def simple_statistics(input_data: List[TA], user_id: int, attribute_id: in
         weekday = int(i.created_at.weekday())
         new_value = tmp[weekday]
         tmp[weekday] = new_value + 1
-    # This probably requires more memory due to duplicate objects stats and later clone
     stats = []
     for key in tmp.keys():
         if not binary:
@@ -108,7 +114,6 @@ async def simple_statistics(input_data: List[TA], user_id: int, attribute_id: in
         else:
             stats.append({"count": tmp[key], "day": DAYS[key]})
     res["time_structure"] = stats  # type: ignore
-    # get earliest date and latest date from crud
     dates = await get_time_horizon(user_id=user_id, attribute_id=attribute_id)
     res["start"] = dates[0]
     res["end"] = dates[1]
@@ -122,23 +127,21 @@ async def collect_report(user_id: int, attribute_id: int) -> dict:
     ts_raw = await filter_entries(
         user_id=user_id, topics=None, start=None, end=None, attribute=attribute_id, comments=None, ts=True
     )
-
     tse = [t.estimation for t in ts_raw if t.estimation is not None]
     tsd = [t.created_at for t in ts_raw if t.created_at is not None]
-
     # (arbitrary number) 2 weeks worth of data can show you some dependencies
     res["recap"] = await simple_statistics(ts_raw, user_id, attribute_id)  # type: ignore
     if len(tse) < 2 * 7 + 1:
         res["enough_data"] = False
         return res
-
     trend = detect_trend(tse)
     res["trend"] = [(trend[i], tsd[i]) for i in range(0, len(trend))]  # type: ignore
     autocor = detect_autocorrelation(tse)
-    res["autocorrelation"] = {
-        "is_autocorrelated": autocor[0],
-        "autocorrelaton_estimates": autocor[1][1:8],
-    }  # type: ignore
+    if autocor[0] and autocor[1] is not None:
+        res["autocorrelation"] = {
+            "is_autocorrelated": autocor[0],
+            "autocorrelaton_estimates": autocor[1][1:8],
+        }  # type: ignore
 
     # multiple correlations
     res["multifactor"] = {}  # type: ignore
@@ -147,6 +150,8 @@ async def collect_report(user_id: int, attribute_id: int) -> dict:
         res["multifactor"]["pearson"] = []  # type: ignore
         # continuous_factor vs continuous_factors correlation
         continuous_factors = await collect_attributes_ids(user_id, binary=False)
+        if None in continuous_factors:
+            continuous_factors.remove(None)  # type: ignore
         for a in continuous_factors:
             is_binary = await is_attribute_binary(a, user_id)
             if is_binary:
@@ -156,11 +161,15 @@ async def collect_report(user_id: int, attribute_id: int) -> dict:
             raw_second = await filter_entries(
                 user_id=user_id, topics=None, start=None, end=None, attribute=factor, comments=None, ts=True
             )
-            res["multifactor"]["pearson"].append({factor: pearson_correlation(ts_raw, raw_second)})  # type: ignore
+            coef = pearson_correlation(ts_raw, raw_second)
+            if not math.isnan(coef):  # type: ignore
+                res["multifactor"]["pearson"].append({factor: coef})  # type: ignore
 
         # continuous_factor vs binary_factors correlation
         res["multifactor"]["pbsr"] = []  # type: ignore
         binary_factors = await collect_attributes_ids(user_id, binary=True)
+        if None in binary_factors:
+            binary_factors.remove(None)  # type:ignore
         for factor in binary_factors:
             raw_second = await filter_entries(
                 user_id=user_id, topics=None, start=None, end=None, attribute=factor, comments=None, ts=True
@@ -170,5 +179,5 @@ async def collect_report(user_id: int, attribute_id: int) -> dict:
     # binary_factors vs binary_factors correlation: TBI
     else:
         return res
-
+    print(f"this is result: {res}")
     return res
